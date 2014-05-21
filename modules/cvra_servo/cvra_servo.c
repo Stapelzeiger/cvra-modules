99825db0e6e2d6dddf854195d2b236b9351b5a22
Florian Reinhard <florian.reinhard@epfl.ch>
2014-05-21 22:06:47 +0200

    function to turn all the servos off.

--- modules/cvra_servo/cvra_servo.c
+++ modules/cvra_servo/cvra_servo.c
@@ -2,8 +2,16 @@
 #include <cvra_servo.h>
 
 void cvra_servo_set(int channel, uint32_t value) {
-    if(channel < 0 || channel >= 6)
+    if(channel < 0 || channel >= CVRA_SERVO_NB_CHANNELS)
         return;
 
     IOWR(SERVOS_BASE, channel, value);
+}
+
+void cvra_servo_all_off(void)
+{
+    int i;
+    for (i = 0; i < CVRA_SERVO_NB_CHANNELS; i++) {
+        cvra_servo_set(i, 0);
+    }
 }
--- modules/cvra_servo/cvra_servo.h
+++ modules/cvra_servo/cvra_servo.h
@@ -3,9 +3,12 @@
 
 #include <aversive.h>
 
+#define CVRA_SERVO_NB_CHANNELS      6
+
 /** Sets the servo pulse width in steps of 100ns.
  * @todo Can we set pulse time > 20ms ? What happens then ? */
 void cvra_servo_set(int channel, uint32_t value);
+void cvra_servo_all_off(void);
 
 
 
